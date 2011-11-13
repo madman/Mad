d68a29b6d2a91dc6596dc6e1de9e8810f365b3d4
madman <madman@madman-pc>
2011-11-13 14:49:37 +0200

    Added base Exception class

---
+++ library/Mad/Exception.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Mad library for Zend Framework
+ * 
+ * 
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://devcomm.org.ua/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@devcomm.org.ua so we can send you a copy immediately.
+ *
+ * @category   Mad
+ * @package    Mad
+ * @copyright  Copyright (c) 2011 Devcomm (http://www.devcomm.org.ua)
+ * @license    http://devcomm.org.ua/license/new-bsd     New BSD License
+ * @version    $Id: $
+ */
+
+/**
+ * Basic exception class for Mad library
+ * 
+ * @category   Mad
+ * @package    Mad
+ * @copyright  Copyright (c) 2011 Devcomm (http://www.devcomm.org.ua)
+ * @license    http://devcomm.org.ua/license/new-bsd     New BSD License
+ */
+class Mad_Exception extends Zend_Exception {
+    
+}
